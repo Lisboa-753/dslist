bf731ab8ef45372a148325b2e50cf68af9659698
glisboa.alves@gmail.com <Gabriel>
2025-01-22 22:14:14 -0300

    Select games

--- src/main/java/com/devsuperior/dslist/controllers/GameListController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameListController.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dslist.dto.GameListDTO;
+import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.services.GameListService;
+import com.devsuperior.dslist.services.GameService;
 
 @RestController //Controlador REST
 @RequestMapping(value = "/lists")  // Caminho base 
@@ -16,10 +19,19 @@ public class GameListController {
 	
 	@Autowired // Injeta dependencia GameService
 	private GameListService gameListService;
+	
+	@Autowired
+	private GameService gameService;
 
 	@GetMapping //Mapeia as requisições GET
 	public List<GameListDTO> findAll(){
 		List<GameListDTO> result = gameListService.findAll();
+		return result;
+	}
+	
+	@GetMapping(value = "/{listId}/games") //Mapeia as requisições GET
+	public List<GameMinDTO> fincByList(@PathVariable Long listId){
+		List<GameMinDTO> result = gameService.findByList(listId);
 		return result;
 	}
 }
